70af1c018e0df88ef2f51f63adc37f1f370f71c1
Jeff <jmulkinj@gmail.com>
2024-02-19 20:59:18 -0800

    home page work

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,20 +1,28 @@
+import Image from "next/image";
 import { options } from "./api/auth/[...nextauth]/options";
 import { getServerSession } from "next-auth/next";
+import hero from "@/public/hero.webp"
 
 export default async function Home() {
   const session = await getServerSession(options);
 
   return (
     <>
-      <main className="bg-[url('/hero.webp')] bg-cover">
+      <main className="">
+        <Image
+          src={hero}
+          sizes="100vw"
+          alt="hero"
+          className="-z-10 absolute w-full blur-sm"
+        />
         {
           session ?
             (<h1>Welcome, {session?.user?.name}</h1>)
             :
             (
               <div className="flex-col text-center ">
-                <h1 className="mb-4">Welcome to FreshForkfuls</h1>
-                <p>Transform Your Meals, Transform Your Life - Start Crafting Nutrition Recipes That Fuel Your Joy!</p>
+                <h1 className="mb-4 drop-shadow">Welcome to FreshForkfuls</h1>
+                <p className="drop-shadow">Transform Your Meals, Transform Your Life - Start Crafting Nutrition Recipes That Fuel Your Joy!</p>
               </div>
             )
         }
