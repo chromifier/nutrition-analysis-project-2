9c0566ccff8f33ed3c955afa4f3c049f826aaa8e
Jeff <jmulkinj@gmail.com>
2024-03-07 19:57:07 -0800

    Create Recipe Form changes
    - added recipe name and datetime to Recipes model
    - added recipe name input
    - added functionality to fetch nutrition facts to store before inserting into DB

--- src/app/CreateRecipe/CreateRecipeForm.js
+++ src/app/CreateRecipe/CreateRecipeForm.js
@@ -8,10 +8,23 @@ const CreateRecipeForm = ({email}) => {
 
         const formData = new FormData(event.target);
         const ingr = formData.get("ingr")?.toString().split(",");
+        let nutritionFactsResponse;
+        const currentDateTime = Date.now();
+
+        const fetch = await fetchNutrition(API_ENDPOINT, ingr).then((data) => {
+			nutritionFactsResponse = [data];
+			console.log([data]);
+		})
+			.catch((error) => {
+				console.log('error in fetchData', error.message);
+			});
 
         axios.post('http://localhost:3000/api/insertRecipe', {
             ingredients: ingr,
-            email: email
+            email: email,
+            nutritionFacts: nutritionFactsResponse,
+            name: recipeName,
+            dateCreate: currentDateTime
           })
           .then(function (response) {
             console.log(response);
@@ -25,6 +38,7 @@ const CreateRecipeForm = ({email}) => {
         <>
             <p>Creating recipe for userId: {email}</p>
             <form className='mt-4 w-full' onSubmit={onSubmit}>
+                <input className="mb-4 p-2 rounded" type="text" name="recipeName" placeholder="My Recipe Name"/>
                 <textarea placeholder="1 cup rice, 6oz grilled chicken breast" id="ingredients" rows={4} className='ingr w-full' type="text" name="ingr" required />
                 <div className='flex flex-row justify-center gap-6'>
                     <button className='submitButton' type="submit">Create</button>
